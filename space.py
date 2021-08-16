4a1c29b25158428c8dba1578e69afc80831542b4
AKINTILO Steeven <steevenakintilo>
2021-08-17 00:52:45 +0200

    Any enemy can shoot

--- space.py
+++ space.py
@@ -171,23 +171,63 @@ def main_loop():
     l3 = [l[0],l[1],l[2],l[3],l[4]]
     l4 = [l[0],l[1],l[2],l[3],l[4]]
     l5 = [l[0],l[1],l[2],l[3],l[4]]
+    
+    x2 = [90,180,270,360]
+    x3 = [90,180,270,360]
+    x4 = [90,180,270,360]
+    x5 = [90,180,270,360]
+    x6 = [90,180,270,360]
+
     d = [1,2,3,4,5]
     r1 = 0
     r2 = 0
     r3 = 0
     r4 = 0
     r5 = 0
+
+    f1 = 0
+    f2 = 0
+    f3 = 0
+    f4 = 0
+    f5 = 0
+    
+    pop11 = 0
+    pop12 = 0
+    pop13 = 0
+
+    pop21 = 0
+    pop22 = 0
+    pop23 = 0
+
+    pop31 = 0
+    pop32 = 0
+    pop33 = 0
+
+    pop41 = 0
+    pop42 = 0
+    pop43 = 0
+
+
+    pop51 = 0
+    pop52 = 0
+    pop53 = 0
+
     touch = 0
+    rdm_y = randint(0,4)
     random_enemy = [320,420,520,620,720]
     rdm_shoot = random_shoot(0,d,5)
+    random_shoot2 = 90
     while running:
+        y1 = [l2[0],l3[0],l4[0],l5[0]]
+        y2 = [l2[1],l3[1],l4[1],l5[1]]
+        y3 = [l2[2],l3[2],l4[2],l5[2]]
+        y4 = [l2[3],l3[3],l4[3],l5[3]]
+        y5 = [l2[4],l3[4],l4[4],l5[4]]
         bullet_speed = bullet_speed + 10
         if damage > 1290:
             quit()
-        #damage = damage + 10
         xpos = print_file("pos_y")
         pos = float(xpos)
-        #space = 0
         for event in pygame.event.get():
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
@@ -210,6 +250,8 @@ def main_loop():
         level1(screen,enemy,l,l2,l3,l4,l5,move_down,move_up)
         last_line = [l5[0],l5[1],l5[2],l5[3],l5[4]]
         move_up = move_up + 2
+        if move_up < 300:
+            damage = 10
         if move_up > 300:
             move_up = 300
         if space != 0:
@@ -224,36 +266,41 @@ def main_loop():
             space = 0
             space = space + 1
         if check_win(last_line) == 1:
-            #print("YOOOOOOOOOOOOOO")
-            #quit()
             end_loop()
         if move_up == 300:
-            pygame.draw.rect(screen, color, pygame.Rect(rdm_shoot + 41,90 + bullet_speed, 5, 10))
+            pygame.draw.rect(screen, color, pygame.Rect(rdm_shoot + 41,bullet_speed + random_shoot2, 5, 10))
+        pygame.draw.rect(screen, color, pygame.Rect(rdm_shoot + 41,random_shoot2, 5, 10))
+        li2 = x2[randint(0,len(x2) - 1)]
+        li3 = x3[randint(0,len(x3) - 1)]
+        li4 = x4[randint(0,len(x4) - 1)]
+        li5 = x5[randint(0,len(x5) - 1)]
+        li6 = x6[randint(0,len(x6) - 1)]
         if bullet_speed > 610:
             bullet_speed = 0
             if check_win(last_line) != 1:
                 rdm_shoot = random_shoot(0,d,len(d))
+                random_shoot2 = li2
             if l5[0] == -9999 and r1 == 0 and check_win(last_line) != 1:
                 r1 = 1
-                d.pop(len(d)-5)
                 rdm_shoot = random_shoot(1,d,len(d))
+                random_shoot2 = li2
             if l5[1] == -9999 and r2 == 0 and check_win(last_line) != 1:
                 r2 = 1
-                d.pop(len(d)-4)
                 rdm_shoot = random_shoot(2,d,len(d))
+                random_shoot2 = li3
             if l5[2] == -9999 and r3 == 0 and check_win(last_line) != 1:
                 r3 = 1
-                d.pop(len(d)-3)
                 rdm_shoot = random_shoot(3,d,len(d))
+                random_shoot2 = li4
             if l5[3] == -9999 and r4 == 0 and check_win(last_line) != 1:
                 r4 = 1
-                d.pop(len(d)-2)
                 rdm_shoot = random_shoot(4,d,len(d))
+                random_shoot2 = li5
             if l5[4] == -9999 and r5 == 0 and check_win(last_line) != 1:
                 r5 = 1
-                d.pop(len(d) - 1)
                 rdm_shoot = random_shoot(5,d,len(d))
-        if rdm_shoot >= int(pos) - 42 and rdm_shoot <= int(pos) + 42 and bullet_speed + 90 >= Y and bullet_speed + 90 <= Y + 67:
+                random_shoot2 = li6
+        if rdm_shoot >= int(pos) - 42 and rdm_shoot <= int(pos) + 42 and bullet_speed + random_shoot2 >= Y and bullet_speed + random_shoot2 <= Y + 67:
             damage = damage + 30
             bullet_speed = 0
         if move_up == 300:
@@ -274,13 +321,79 @@ def main_loop():
                     l5[int(i/2)] = -9999
                     space = 0
                     fire_speed = 10
+        if y1[0] == -9999 and pop11 == 0:
+            x2.pop(len(x2) - 1)
+            pop11 = 1
+        if y1[1] == -9999 and pop12 == 0:
+            x2.pop(len(x2) - 1)
+            pop12 = 1
+        if y1[2] == -9999 and pop13 == 0:
+            x2.pop(len(x2) - 1)
+            pop13 = 1
+        
+        if y2[0] == -9999 and pop21 == 0:
+            x3.pop(len(x3) - 1)
+            pop21 = 1
+        if y2[1] == -9999 and pop22 == 0:
+            x3.pop(len(x3) - 1)
+            pop22 = 1
+        if y2[2] == -9999 and pop23 == 0:
+            x3.pop(len(x3) - 1)
+            pop23 = 1
+        
+        if y3[0] == -9999 and pop31 == 0:
+            x4.pop(len(x4) - 1)
+            pop31 = 1
+        if y3[1] == -9999 and pop32 == 0:
+            x4.pop(len(x4) - 1)
+            pop32 = 1
+        if y3[2] == -9999 and pop33 == 0:
+            x4.pop(len(x4) - 1)
+            pop33 = 1
+        
+        if y4[0] == -9999 and pop41 == 0:
+            x5.pop(len(x5) - 1)
+            pop41 = 1
+        if y4[1] == -9999 and pop42 == 0:
+            x5.pop(len(x5) - 1)
+            pop42 = 1
+        if y4[2] == -9999 and pop43 == 0:
+            x5.pop(len(x5) - 1)
+            pop43 = 1
+        
+        if y5[0] == -9999 and pop51 == 0:
+            x6.pop(len(x6) - 1)
+            pop51 = 1
+        if y5[1] == -9999 and pop52 == 0:
+            x6.pop(len(x6) - 1)
+            pop52 = 1
+        if y5[2] == -9999 and pop53 == 0:
+            x6.pop(len(x6) - 1)
+            pop53 = 1
+        
         pygame.draw.rect(screen, color, pygame.Rect(0,705,1290,30))
         pygame.draw.rect(screen, color2, pygame.Rect(0,705,1290 - damage,30))
         pygame.display.flip()
         clock.tick(30)
-        #print(rdm_shoot,pos)
-        #print(rdm_shoot,pos,90 + bullet_speed)
-        #print(90+bullet_speed,Y)
+        #print(y1,y2,y3,y4,y5)
+
+        if l5[0] == -9999 and f1 == 0:
+            f1 = 1
+            d.pop(0)
+        if l5[1] == -9999 and f2 == 0:
+            f2 = 1
+            d.pop(len(d) - 2)
+        if l5[2] == -9999 and f3 == 0:
+            f3 = 1
+            d.pop(len(d) - 3)
+        if l5[3] == -9999 and f4 == 0:
+            f4 = 1
+            d.pop(len(d) - 4)
+        if l5[4] == -9999 and f5 == 0:
+            f5 = 1
+            d.pop(len(d) - 5)
+        
+        #print(l5,d)
 write_id("pos_y","100")
 system("clear")
 main_loop()
